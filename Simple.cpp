ac89b813f0f0d135de351ebb6975d9e193d94f72
DasPanda <kennethrstruck@gmail.com>
2024-05-21 10:45:24 -0500

    Add files via upload
    Adding Lab 0 sample cpp and instructions PDF.

---
+++ Simple.cpp
@@ -0,0 +1,100 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;\red239\green239\blue239;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c94902\c94902\c94902;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww11340\viewh13200\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 \expnd0\expndtw0\kerning0
+#include <iostream>\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 using namespace std;\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 int main()\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 \{\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 string myMessage = "Message for you!";\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 string yourMessage;\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 int repeat = 5;\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 for(int i = 0; i < repeat; i++)\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 \{\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 cout << myMessage << endl;\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 \}\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 cout << "What is your message:";\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 cin >> yourMessage;\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 cout << "You said:" << yourMessage << endl;\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 return 0;\cb1 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf0 \cb2 \}}
